ee7bc0f8d0a19500ceb255d18384ddb1481ed7ac
Mario Del Gaudio <mdg0000@gmail.com>
2023-07-21 22:29:05 +0200

    stuff

--- common/qemu-wrap.sh
+++ common/qemu-wrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-socat TCP-LISTEN:1337,fork,reuseaddr EXEC:"timeout 120 qemu-system-i386 -display none -serial stdio -drive file=/tmp/boot.bin\,format=raw -snapshot",pty,stderr
+socat TCP-LISTEN:1337,fork,reuseaddr EXEC:"timeout 120 cpulimit -f -z -m -l 20 -- qemu-system-i386 -display none -serial stdio -drive file=/tmp/boot.bin\,format=raw -snapshot",pty,stderr
--- gdb-qemu/gdb.sh
+++ gdb-qemu/gdb.sh
@@ -1,2 +1,2 @@
 #gdb -ex 'gef-remote localhost:1234'     -ex 'set architecture i8086'  -ex 'set tdesc filename target.xml'   -ex 'break *0x7c00'     -ex 'continue'
- gdb -ex 'target remote localhost:1234'     -ex 'set architecture i8086'  -ex 'set tdesc filename target.xml'   -ex 'break *0x7c00'     -ex 'continue'
+gdb -ex 'target remote localhost:1234'     -ex 'set architecture i8086'  -ex 'set tdesc filename target.xml'   #-ex 'break *0x7c00'     -ex 'continue'
